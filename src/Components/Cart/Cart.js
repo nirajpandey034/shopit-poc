ca8e9ec9ab34d62372433dd60aac4f7f3643ad38
Niraj Kumar Pandey <nirajpandey034@gmail.com>
2022-08-31 16:09:12 +0530

    empty cart fixed

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,17 +1,46 @@
 import React, { useState, useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { MyContext } from '../ContextProvider';
 
 import CartTable from './CartTable';
 
+import { Typography } from '@mui/material';
+
 function Cart() {
   const { state } = useContext(MyContext);
+  const navigate = useNavigate();
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     if (state.products.length > 0) setProducts(state.products);
   }, [products, state.products]);
   return (
-    <>{products?.length !== 0 ? <CartTable products={products} /> : null}</>
+    <>
+      {products?.length !== 0 ? (
+        <CartTable products={products} />
+      ) : (
+        <>
+          <Typography>
+            Nothing in here. Add something awesome in your cart
+          </Typography>
+          <Typography
+            variant="body1"
+            element="a"
+            style={{
+              textAlign: 'center',
+              color: 'blue',
+              textDecoration: 'underline',
+              cursor: 'pointer',
+            }}
+            onClick={() => {
+              navigate('/shop');
+            }}
+          >
+            Keep Shopping
+          </Typography>
+        </>
+      )}
+    </>
   );
 }
 
